392ebf580b3cdf150fbf64d43eb17e502c33c658
Gregor de Cillia <de.cillia.gregor@gmail.com>
2025-06-19 09:58:16 +0200

    add tooltip

--- treemap/script.js
+++ treemap/script.js
@@ -6,6 +6,19 @@
         height: 800,
         legend_height: 50
     }
+    handleTooltipShow = (e, d) => {
+        const value = parseInt(d.data.value)
+            .toLocaleString().replace(/,/g, " ");
+        tooltip.innerHTML = `
+            <div><b>Name</b>: ${d.data.name}</div>
+            <div><b>Category</b>: ${d.data.category}</div>
+            <div><b>Value</b>: ${value}</div>
+        `
+        tooltip.setAttribute("data-value", d.data.value)
+        tooltip.style.display = "block"
+        tooltip.style.top  = e.clientY + 'px';
+        tooltip.style.left = e.clientX + 'px';
+    }
     const color = d => `var(--col-${d.data.name})`
     var svg = d3.select("#chart")
         .append("svg")
@@ -33,7 +46,12 @@
         .attr("data-name", d => d.data.name)
         .attr("data-value", d => d.data.value)
         .attr("data-category", d => d.data.category)
-        .attr("fill", d => color(d.parent));
+        .attr("fill", d => color(d.parent))
+        //.on("mousemove", handleTooltipShow)
+        .on("mouseover", handleTooltipShow)
+        .on("mouseleave", (e) => {
+            tooltip.style.display = "none"
+        });
     svg
         .selectAll("foreignObject")
         .data(root.leaves())
@@ -53,7 +71,7 @@
         .enter()
         .append('rect')
         .attr('y', 800 + 20)
-        .attr('x', (_d, i) => 70 + 150*i)
+        .attr('x', (_d, i) => 70 + 150 * i)
         .attr('fill', color)
         .attr('width', '30')
         .attr('height', '30')
@@ -65,7 +83,7 @@
         .enter()
         .append('text')
         .attr('y', 800 + 42)
-        .attr('x', (d, i) => 110 + 150*i)
+        .attr('x', (_d, i) => 110 + 150 * i)
         .text(d => d.data.name)
         .attr('fill', '#ddd')
         .attr("font-size", "20px");
